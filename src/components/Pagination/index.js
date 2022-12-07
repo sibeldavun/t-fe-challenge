2d8f0783bfcca5b54e5d5740c3aff742ec0185a7
sibeldavun <sibeldavun.96@gmail.com>
2022-12-07 16:40:34 +0300

    refactor pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -7,6 +7,49 @@ function Pagination({ pageCount }) {
     let [searchParams, setSearchParams] = useSearchParams();
     const [pageNumber, setPageNumber] = useState(1);
 
+    const pages = [];
+
+    if (pageCount > 6) {
+        for (let i = 1; i <= 3; i++) {
+            pages.push(i);
+        }
+
+        if (pageNumber - 2 > 4 && pageNumber + 2 < pageCount - 3) {
+            pages.push(0);
+            for (let i = pageNumber - 2; i <= pageNumber + 2; i++) {
+                pages.push(i);
+            }
+            pages.push(0)
+        }
+        else if (pageNumber - 2 > 4) { // 1,2,3,0, 6,7,8,9,10 ,11,12,13
+            pages.push(0);
+            for (let i = pageNumber - 2; i <= pageCount - 3; i++) {
+                pages.push(i);
+            }
+        }
+        else if (pageNumber + 2 < pageCount - 3) {  // 1,2,3,    4,5,6,7,8,0,   11,12,13
+            for (let i = 4; i <= pageNumber + 2; i++) {
+                pages.push(i);
+            }
+            pages.push(0);
+        }
+        else {
+            for (let i = 4; i <= pageCount - 3; i++) {
+                pages.push(i);
+            }
+        }
+
+        for (let i = pageCount - 2; i <= pageCount; i++) {
+            pages.push(i);
+        }
+    }
+    else {
+        for (let i = 1; i <= pageCount; i++) {
+            pages.push(i);
+        }
+    }
+
+
     useEffect(() => {
         if (pageNumber === 1) {
             searchParams.delete("page")
@@ -25,9 +68,31 @@ function Pagination({ pageCount }) {
         setPageNumber(pageNumber - 1)
     }
 
+    const clickPage = (i) => {
+        if (pageNumber === i) return
+        setPageNumber(i)
+
+    }
+
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
             <button hidden={pageNumber === 1 ? true : false} className={styles.paginationBtn} onClick={decrease}>Previous</button>
+            <ul style={{ display: "flex", flexDirection: "row" }}>
+                {
+                    pages.map((page, index) => {
+                        if (page === 0) {
+                            return <li key={index}>...</li>
+                        }
+                        else {
+                            return <li key={index} onClick={()=>{clickPage(page)}} className={`${styles.page} ${page === pageNumber ? styles.activePage : ""}`}>
+                                {page}
+                            </li>
+                        }
+
+                    })
+                }
+            </ul>
+
             <button hidden={pageNumber >= pageCount ? true : false} className={styles.paginationBtn} onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
         </div>
     )
